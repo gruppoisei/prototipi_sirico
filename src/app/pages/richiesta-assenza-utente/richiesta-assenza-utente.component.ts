23a2e7e684bc3a481ff2664290a2aa678c7ee546
luca <scagliottil2609@icloud.com>
2024-03-08 15:17:33 +0100

    modifiche richiesta assenza

--- src/app/pages/richiesta-assenza-utente/richiesta-assenza-utente.component.ts
+++ src/app/pages/richiesta-assenza-utente/richiesta-assenza-utente.component.ts
@@ -24,19 +24,17 @@ export class RichiestaAssenzaUtenteComponent {
   richiesta: Richiesta[] = [];
   tipiRichiesta: [{ ritrTiporichiestaassenzaid: number; ritrDescrizioneassenza: string }] | undefined;
   formData: Richiesta = {
-    //  RiasFkPersonaid: null,
     RiasFkTiporichiesta: 1,
-    //  RiasFkResponsabileidApprovazione: null,
-    //  RiasApprovato: false,
     RiasDataorainizioassenza: '',
     RiasDataorafineassenza: '',
     RiasNote: '',
     RiasSysuser: 'Edo',
-    //  RiasSysdate: '',
-    //  RiasFlagattivo: false,
-    //AndpDocumentipersonas: '',
     fileName: ''
   };
+  dateinizioFileTouched: boolean = false;
+  datefineFileTouched: boolean = false;
+  orainizioFileTouched: boolean = false;
+  orafineFileTouched: boolean = false;
 
   @ViewChild('myFile')
   myInputFile!: ElementRef;
@@ -57,7 +55,6 @@ export class RichiestaAssenzaUtenteComponent {
   }
 
   generaOre() {
-    // Genera un array di ore con intervallo di 30 minuti
     for (let i = 0; i < 24; i++) {
       this.ore.push(`${('0' + i).slice(-2)}:00`);
       this.ore.push(`${('0' + i).slice(-2)}:30`);
@@ -68,6 +65,7 @@ export class RichiestaAssenzaUtenteComponent {
     this.formData.RiasDataorainizioassenza = this.DataInizio + 'T' + this.OraInizio + ':00';
     this.formData.RiasDataorafineassenza = this.DataFine + 'T' + this.OraFine + ':00';
     this.inviaRichiesta(this.formData);
+    this.resetForm();
   }
 
   inviaRichiesta(body: Richiesta){
@@ -77,7 +75,6 @@ export class RichiestaAssenzaUtenteComponent {
         alert(response);
       },
       (error: any) => {
-        //alert(error);   //gestiamolo in maniera diversa, il form non può essere inviato se mancano campi
         console.error('errore nell\'invio della richiesta: ', error);
       }
     )
@@ -109,34 +106,13 @@ export class RichiestaAssenzaUtenteComponent {
   }
   
   chiudiForm() {
-    console.log('Chiusura della finestra');
+    if (confirm('La pagina verrà chiusa e i dati inseriti verranno cancellati. Si desidera procedere?'))
     this.router.navigate(['/homepage']);
   }
 
   eliminaRichiesta() {
     if (confirm('I campi verranno resettati. Si desidera procedere?')) {
-      console.log('Elimina premuto, pulisco campi ');
-      this.OraFine = null;
-      this.OraInizio = null;
-      this.DataFine = '';
-      this.DataInizio = '';
-      this.formData = {
-        //  RiasRichiestaassenzaid: null,
-        //  RiasFkPersonaid: null,
-        RiasFkTiporichiesta: 0,
-        //  RiasFkResponsabileidApprovazione: null,
-        //  RiasApprovato: false,
-        RiasDataorainizioassenza: '',
-        RiasDataorafineassenza: '',
-        RiasNote: '',
-        RiasSysuser: 'Edo',
-        //  RiasSysdate: '',
-        //  RiasFlagattivo: false,
-        //AndpDocumentipersonas: '',
-        fileName: ''
-      };
-      this.resetDoc();
-
+      this.resetForm();
     } else {
       // Do nothing!
       console.log('Operazione annullata');
@@ -147,6 +123,26 @@ export class RichiestaAssenzaUtenteComponent {
   resetDoc() {
     console.log(this.myInputFile.nativeElement.files);
     this.myInputFile.nativeElement.value = "";
+  }
+
+  resetForm() {
+    this.OraFine = null;
+    this.OraInizio = null;
+    this.DataFine = '';
+    this.DataInizio = '';
+    this.formData = {
+      RiasFkTiporichiesta: 0,
+      RiasDataorainizioassenza: '',
+      RiasDataorafineassenza: '',
+      RiasNote: '',
+      RiasSysuser: 'Edo',
+      fileName: ''
+    };
+    this.resetDoc();
+    this.dateinizioFileTouched = false;
+    this.datefineFileTouched = false;
+    this.orainizioFileTouched = false;
+    this.orafineFileTouched = false;
   }
 
   checkFormValidity(): boolean {
@@ -160,12 +156,12 @@ export class RichiestaAssenzaUtenteComponent {
     );
   }
 
-checkDateTimeValidity(): boolean {
-  const startDate = new Date(this.DataInizio + 'T' + this.OraInizio);
-  const endDate = new Date(this.DataFine + 'T' + this.OraFine);
+  checkDateTimeValidity(): boolean {
+    const startDate = new Date(this.DataInizio + 'T' + this.OraInizio);
+    const endDate = new Date(this.DataFine + 'T' + this.OraFine);
 
-  return startDate < endDate;
-}
+    return ((startDate > endDate) && (this.dateinizioFileTouched && this.datefineFileTouched && this.orainizioFileTouched && this.orafineFileTouched));
+  }
 
 
 }
