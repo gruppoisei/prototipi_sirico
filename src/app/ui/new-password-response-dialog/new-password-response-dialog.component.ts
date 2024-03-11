a736904234d2a2b8ab8f35ec792f9330855eed8b
Alessandro <deangelisalessandro2000@gmail.com>
2024-03-11 12:25:01 +0100

    Ricreato nuovamente NewPasswordResponseDialogComponent

---
+++ src/app/ui/new-password-response-dialog/new-password-response-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-password-response-dialog',
+  templateUrl: './new-password-response-dialog.component.html',
+  styleUrl: './new-password-response-dialog.component.scss'
+})
+export class NewPasswordResponseDialogComponent {
+
+}
