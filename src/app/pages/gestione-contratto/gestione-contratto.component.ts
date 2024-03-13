fd05c365ded7745c00a5b3c9e0e91dc268f3661f
giacomomarchiori <159167513+giacomomarchiori@users.noreply.github.com>
2024-03-13 17:15:09 +0100

    creazione gestione-contratto.component

---
+++ src/app/pages/gestione-contratto/gestione-contratto.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestione-contratto',
+  templateUrl: './gestione-contratto.component.html',
+  styleUrl: './gestione-contratto.component.scss'
+})
+export class GestioneContrattoComponent {
+
+  eliminaForm() {
+throw new Error('Method not implemented.');
+}
+
+  chiudiForm() {
+throw new Error('Method not implemented.');
+}
+
+
+}
