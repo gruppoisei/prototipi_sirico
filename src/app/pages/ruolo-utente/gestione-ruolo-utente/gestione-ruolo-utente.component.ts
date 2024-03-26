5ac75caaa72097065aa865923b2f7f80febee311
LucaScagliotti <luca.scagliotti@gruppoisei.it>
2024-03-26 12:58:36 +0100

    Gestione Amministrazione: implementazione pagine di gestione ruolo e gestione ruolo-utente

--- src/app/pages/ruolo-utente/gestione-ruolo-utente/gestione-ruolo-utente.component.ts
+++ src/app/pages/ruolo-utente/gestione-ruolo-utente/gestione-ruolo-utente.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-gestione-ruolo-utente',
   templateUrl: './gestione-ruolo-utente.component.html',
-  styleUrl: './gestione-ruolo-utente.component.scss',
+  styleUrl: './gestione-ruolo-utente.component.scss'
 })
-export class GestioneRuoloUtenteComponent {
-  ricercaFiltrata(arg0: any) {
-    throw new Error('Method not implemented.');
-  }
-  formData: any;
-  output_ricercaFiltrata: boolean = false;
 
+export class GestioneRuoloUtenteComponent {
+  formData: any;
   closeForm() {
-    throw new Error('Method not implemented.');
+  throw new Error('Method not implemented.');
   }
   clearSearch() {
-    throw new Error('Method not implemented.');
+  throw new Error('Method not implemented.');
   }
+  ricercaFiltrata(arg0: any) {
+  throw new Error('Method not implemented.');
+  }
+  output_ricercaFiltrata: any;
+  ruoli: any;
   deleteRuoloPerUtente(arg0: any) {
     throw new Error('Method not implemented.');
   }
