e0d3b0ab06fa35661876917277d9156540cf73a6
Edoardo <ghirardello.edoardo@gmail.com>
2024-04-02 18:20:16 +0200

    creazione pagina creazione utente

---
+++ src/app/dto/request/nuovoUtenteRuolo.ts
@@ -0,0 +1,14 @@
+
+export class NuovoUtenteRequest
+{
+   
+    syurIdutenteruolo? = 0
+    syurFkSyutIdutente? = 0
+    syurFkSyruIdruolosys = 0
+    syurSysuser = "Romolo"
+    syurFlagattivo = 0
+    syutFkAnpePersonaid = 0
+    syutUserName? = ""
+
+}
+
