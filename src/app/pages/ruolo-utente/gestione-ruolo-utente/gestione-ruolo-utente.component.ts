22d02889f2ff830ca4931750fef89e7cabb5887d
luca <luca.scagliotti@gruppoisei.it>
2024-04-03 15:56:50 +0200

    Amministraziobe Ruolo: in gestione ruolo utente ho modificato la ricerca per ruolo invece che per codice fiscale

--- src/app/pages/ruolo-utente/gestione-ruolo-utente/gestione-ruolo-utente.component.ts
+++ src/app/pages/ruolo-utente/gestione-ruolo-utente/gestione-ruolo-utente.component.ts
@@ -1,51 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
-
+ 
+ 
 @Component({
   selector: 'app-gestione-ruolo-utente',
   templateUrl: './gestione-ruolo-utente.component.html',
   styleUrls: ['./gestione-ruolo-utente.component.scss']
 })
-export class GestioneRuoloUtenteComponent {
+export class GestioneRuoloUtenteComponent implements OnInit {
   formData: any = {};
   output_ricercaFiltrata: any;
   utenti: any;
-
-  constructor(private router: Router, private http: HttpClient) { 
+  ruoli: { syruIdruolosys: number, syruDescruolosys: string }[] = [];
+  risultati: any; 
+ 
+  constructor(private router: Router, private http: HttpClient) {
     this.clearSearch();
   }
-
-  ricercaPersonaFiltrata(nome: string, cognome: string, codiceFiscale: string) {
+ 
+  ricercaPersonaFiltrata(nome: string, cognome: string, ruolo: number) {
     const params = {
-      nome: nome, 
-      cognome: cognome,
-      codiceFiscale: codiceFiscale
+      Nome: nome,
+      Cognome: cognome,
+      Ruolo: ruolo
     };
-    return this.http.get<any[]>('http://localhost:5143/Persona/GetPersoneByParams', { params });
+    this.risultati = this.http.get<any[]>('http://localhost:5143/Persona/GetPersoneByParams', { params });
+    console.log(this.risultati);
+    return this.risultati;
   }
-
+ 
+  ngOnInit(): void {
+    console.log("oninit");
+    this.caricaRuoli();
+    console.log(this.ruoli);
+    console.log("qui");
+  }
+ 
+  caricaRuoli() {
+    this.http.get<any[]>('http://localhost:5143/AmministrazioneRuolo/GetRuoli')
+      .subscribe((dati) => {
+        this.ruoli = dati;
+      });
+  }
+ 
   clearSearch() {
-    this.formData.AnpeNome = '';
-    this.formData.AnpeCognome = '';
-    this.formData.AnpeCodiceFiscale = '';
+    this.formData.Nome = '';
+    this.formData.Cognome = '';
+    this.formData.Ruolo = '';
   }
-
+ 
   closeForm() {
     this.router.navigate(['/homepage']);
   }  
-
+ 
   modificaRuolo(id: number) {
     this.router.navigate(['/insert-ruolo-utente']);
   }
-
+ 
   deleteRuolo(id: number) {
     //delete da implementare
   }
-
+ 
   onCercaButtonClick() {
     console.log('Cerca button clicked!');
     console.log('FormData:', this.formData);
   }
 }
+ 
