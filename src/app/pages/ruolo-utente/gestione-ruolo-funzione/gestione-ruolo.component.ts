36bc4dece37b87de2ebbb14f63f1449d3b94c27b
luca <luca.scagliotti@gruppoisei.it>
2024-04-05 12:06:10 +0200

    inizio implementazione gestione ruolo funzione

--- src/app/pages/ruolo-utente/gestione-ruolo-funzione/gestione-ruolo.component.ts
+++ src/app/pages/ruolo-utente/gestione-ruolo-funzione/gestione-ruolo.component.ts
@@ -1,28 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InsertUtenteService } from '../../../service/insert-utente.service';
 
 @Component({
   selector: 'app-gestione-ruolo',
   templateUrl: './gestione-ruolo.component.html',
-  styleUrl: './gestione-ruolo.component.scss'
+  styleUrls: ['./gestione-ruolo.component.scss']
 })
-export class GestioneRuoloComponent {
-formData: any;
-closeForm() {
-throw new Error('Method not implemented.');
-}
-clearSearch() {
-throw new Error('Method not implemented.');
-}
-ricercaFiltrata(arg0: any) {
-throw new Error('Method not implemented.');
-}
-output_ricercaFiltrata: any;
-ruoli: any;
-deleteRuolo(arg0: any) {
-throw new Error('Method not implemented.');
-}
-modificaRuolo(arg0: any) {
-throw new Error('Method not implemented.');
-}
+export class GestioneRuoloComponent implements OnInit {
+  formData: any;
+  output_ricercaFiltrata: any;
+  ruoli: any;
 
+  constructor(private insertUtenteService: InsertUtenteService) { }
+
+  ngOnInit(): void {
+    this.caricaRuoli();
+  }
+
+  caricaRuoli() {
+    this.insertUtenteService.GetRuoli().subscribe(
+      (data) => {
+        this.ruoli = data;
+        this.output_ricercaFiltrata = true;
+      },
+      (error) => {
+        console.log('Si è verificato un errore nel caricamento dei ruoli:', error);
+      }
+    );
+  }
+
+  clearSearch() {
+    throw new Error('Method not implemented.');
+  }
+  ricercaFiltrata(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  deleteRuolo(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  modificaRuolo(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  closeForm() {
+
+    throw new Error('Method not implemented.');
+  }
 }
