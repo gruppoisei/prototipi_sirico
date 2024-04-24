02c470a7664a301ff39c8fbfd39c2ea7c0baea34
AlessandroDeA <deangelisalessandro2000@gmail.com>
2024-04-24 10:47:15 +0200

    creato commessa.service aggiunto nell'app-routing salva-commessa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ import { basicUser, notLogged } from './guard/auth.guard';
 import { SelezionaRuoloDialogComponent } from './pages/login-box/seleziona-ruolo-dialog/seleziona-ruolo-dialog.component';
 import { LoginBoxComponent } from './pages/login-box/login-box/login-box.component';
 import { GestioneCommessaComponent } from './pages/commessa-box/gestione-commessa/gestione-commessa.component';
+import { SalvaCommessaComponent } from './pages/commessa-box/salva-commessa/salva-commessa.component';
 
 const routes: Routes = [
   {
@@ -72,6 +73,10 @@ const routes: Routes = [
     component: CalendarioComponent,
     canActivate : [basicUser]
   },
+  {
+    path: 'salva-commessa',
+    component: SalvaCommessaComponent,
+  }
 
   // {
   //   path: 'insertutente',
--- src/app/pages/commessa-box/salva-commessa/salva-commessa.component.ts
+++ src/app/pages/commessa-box/salva-commessa/salva-commessa.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommessaService } from '../../../service/commessa.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-salva-commessa',
   templateUrl: './salva-commessa.component.html',
   styleUrl: './salva-commessa.component.scss'
 })
-export class SalvaCommessaComponent {
+
+export class SalvaCommessaComponent implements OnInit{
+
+  data : string;
+  commessaForm !: FormGroup
+  
+
+  constructor(private commessaService : CommessaService, private location : Location, private formBuilder : FormBuilder){
+    const router = inject(Router)
+
+    this.data = this.commessaService.getTiolo();
+    if(this.data === ''){
+      /*router.navigate(['/Segreteria/gestione-dipendente'])*/  
+      }
+  }
+
+ngOnInit(): void {
+    this.commessaForm = this.formBuilder.group({
+      idCommessa : [0],
+      DescComm : ['', Validators.required],
+      dataInizio : ['', Validators.required],
+      dataFine : [''],
+      flagAttivo : [true, Validators.required]
+    })
+  }
+
+goBack() : void {
+  this.location.back();
+}
+
+clearSearch() {
+throw new Error('Method not implemented.');
+}
 
 }
