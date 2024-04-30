06ce0c248c8e25ad22857706cae0f89ab79bc6e4
CarmeloParrino <carmelo.parrino@gruppoisei.it>
2024-04-30 10:05:07 +0200

    apportate modifiche a gestione-commessa

--- src/app/pages/commessa-box/gestione-commessa/gestione-commessa.component.ts
+++ src/app/pages/commessa-box/gestione-commessa/gestione-commessa.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-gestione-commessa',
   templateUrl: './gestione-commessa.component.html',
   styleUrl: './gestione-commessa.component.scss'
 })
-export class GestioneCommessaComponent {
+export class GestioneCommessaComponent implements OnInit{
+
+  ricercaForm!: FormGroup;
+  constructor(private fb : FormBuilder, private dialog : MatDialog){}
+  datiCommessa: any[] = []
+  idCommessa : number | null = null;
+
+  ngOnInit(): void {
+    this.ricercaForm = this.fb.group({
+      DescCommessa: [''],
+      DataInizio: [''],
+      DataFine: [''],
+      FlagAttivo: ['']
+    })
+  }
+
+  getCommessa(commessaId: number)
+  {
+    /*this.idCommessa = commessaId;
+    this.commessaService.getCommessaById(this.idCommessa)*/
+  }
+
+  setTitoloNuovaCommessa()
+  {
+    /*this.commessaService.setTitolo('Inserimento nuova commessa')*/
+  }
+
+    clearSearch()
+  {
+    this.ricercaForm.reset();
+  }
+
+  ricercaFiltrata()
+  {
+    
+  }
 
 }
