004e5a6b0a8606375db7b21323784dc90e404906
giacomomarchiori <159167513+giacomomarchiori@users.noreply.github.com>
2024-05-13 16:36:10 +0200

    modifica gestione e insert cliente

--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -15,30 +15,30 @@ export class ClienteService {
     }), responseType: 'text'
   };
 
-  private titolo : BehaviorSubject<string> = new BehaviorSubject<string>('')
-  
+  private titolo: BehaviorSubject<string> = new BehaviorSubject<string>('')
+
   // clienteSubject = new BehaviorSubject<any>(null);
   // cliente$ = this.clienteSubject.asObservable();
 
-idCliente!: number
-idCliente$: BehaviorSubject<number | undefined> = new BehaviorSubject<number | undefined>(this.idCliente);
+  idCliente!: number
+  idCliente$: BehaviorSubject<number | undefined> = new BehaviorSubject<number | undefined>(this.idCliente);
 
   baseUrl = 'http://localhost:5143/Cliente/'
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  
-  setTitolo(titolo : string){
+
+  setTitolo(titolo: string) {
     this.titolo.next(titolo);
   }
 
-  getTiolo(){
+  getTitolo() {
     return this.titolo.getValue();
   }
 
-  getClienteById(){
+  getClienteById() {
     return this.http.get<any>(`${this.baseUrl}GetClienteById/${this.idCliente$.value}`); //, this.httpOptions);
   }
-  
+
   // getClienteById(idCliente : number){
   //   return this.http.get<any>(`${this.baseUrl}GetClienteById/${idCliente}`).pipe(
   //     retry(3)).subscribe(
@@ -65,29 +65,29 @@ idCliente$: BehaviorSubject<number | undefined> = new BehaviorSubject<number | u
     */
     return this.http.post<any>(`${this.baseUrl}NuovoCliente`, datiCliente);
   }
-  
-  disabilitaClienteById(idCliente : number){
-    return this.http.put<any>(`${this.baseUrl}DisabilitaClienteById/${idCliente}`,{})
+
+  disabilitaClienteById(idCliente: number) {
+    return this.http.put<any>(`${this.baseUrl}DisabilitaClienteById/${idCliente}`, {})
+  }
+
+  riattivaClienteById(idCliente: number) {
+    return this.http.put<any>(`${this.baseUrl}RiattivaClienteById/${idCliente}`, {})
   }
 
   // clearClienteSubject(){
   //   this.clienteSubject.next(null);
   // }
 
-  getVistaClienteFiltrata(queryParams: ricercaCliente) : Observable<any>
-  {
+  getVistaClienteFiltrata(queryParams: ricercaCliente): Observable<any> {
     const params = this.creaHttpParams(queryParams)
-    return this.http.get<any>(this.baseUrl + 'GetClienteFiltro', {params: params})
+    return this.http.get<any>(this.baseUrl + 'GetClienteFiltro', { params: params })
   }
-  
-  private creaHttpParams(parametri: { [x: string]: any; hasOwnProperty: (arg0: string) => any; }):HttpParams
-  {
+
+  private creaHttpParams(parametri: { [x: string]: any; hasOwnProperty: (arg0: string) => any; }): HttpParams {
     let httpParams = new HttpParams();
 
-    for(const key in parametri)
-    {
-      if(parametri.hasOwnProperty(key) && parametri[key] !==null && parametri[key] !== undefined)
-      {
+    for (const key in parametri) {
+      if (parametri.hasOwnProperty(key) && parametri[key] !== null && parametri[key] !== undefined) {
         httpParams = httpParams.set(key, String(parametri[key]));
       }
     }
